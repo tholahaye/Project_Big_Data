bb47447d8fdde1b8e9a9f9c088ba1c5a6a202a3e
thomas lahaye <tholahaye@laposte.net>
2023-08-23 17:02:36 +0200

    Map reduce 2.1 et 2.2
    Ajout des ficher mapper_2.py, reducer_2-1.py et reducer_2-2.py

---
+++ reducer_2-1.py
@@ -0,0 +1,50 @@
+import sys
+import pandas as pd
+
+#print('codcli\tcodecde\tvillecli\tcpcli\tnbcoli\ttimbrecde\tpoint')
+
+data =[]
+current_codecde = None
+current_point = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    mapper = line.split('\t')
+
+    codcli = mapper[0]
+    codecde = mapper[1]
+    villecli = mapper[2]
+    cpcli = mapper[3]
+
+    try:
+        nbcoli = float(mapper[4])
+    except ValueError:
+        continue
+
+    timbrecde = mapper[5]
+    date = mapper[6]
+
+    try:
+        point = float(mapper[8])*float(mapper[7])
+    except ValueError:
+        continue
+
+    if current_codecde is None:
+        current_codecde = codecde
+
+    if codecde != current_codecde:
+        data.append((codcli, current_codecde, villecli, cpcli, nbcoli, timbrecde, current_point))
+        current_codecde = codecde
+        current_point = 0
+
+    current_point += point
+
+if current_codecde is not None:
+    data.append((codcli, current_codecde, villecli, cpcli, nbcoli, timbrecde, current_point))
+
+df = pd.DataFrame(data, columns=['codcli', 'codecde', 'villecli', 'cpcli', 'nbcoli', 'timbrecde', 'point'])
+
+filtered_df = df.sort_values(by='point', ascending=False)
+top100 = filtered_df.head(100)
+
+top100.to_excel('top100.xlsx', index=False)
